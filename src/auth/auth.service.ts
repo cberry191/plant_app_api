13d26b40ca1162c7d6133cdad7c0c698c5a8a492
Christopher Berry <cberry191@hotmail.com>
2022-10-17 06:06:18 +1000

    added JWT on login and signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,10 +4,17 @@ import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService, private jwt: JwtService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
+
+  // LOG IN
 
   async login(dto: AuthDto) {
     // find user by email
@@ -16,16 +23,21 @@ export class AuthService {
         email: dto.email,
       },
     });
+
     // throw exception if no match
     if (!user) throw new ForbiddenException('Account not found');
+
     // compare passowrd
     const pwMatches = await argon.verify(user.hash, dto.password);
+
     // throw exception if no match
     if (!pwMatches) throw new ForbiddenException('Bad password');
-    // return user
-    delete user.hash;
-    return user;
+
+    // return signed token generated from id and email
+    return this.signToken(user.id, user.email);
   }
+
+  // SIGN UP
 
   async signup(dto: AuthDto) {
     try {
@@ -40,9 +52,7 @@ export class AuthService {
         },
       });
 
-      delete user.hash;
-      // return saved user
-      return user;
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -52,5 +62,32 @@ export class AuthService {
         }
       }
     }
+  }
+
+  // SIGN TOKEN
+  // Generates access_token for user to pass to API upon requests
+
+  async signToken(
+    userId: Number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    // payload object to store user info
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    // jwt secret from .env
+    const secret = this.config.get('JWT_SECRET');
+
+    // smash together into signed token object
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
